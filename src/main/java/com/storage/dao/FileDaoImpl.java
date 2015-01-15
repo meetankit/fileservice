0d4174407316d41aa92011c571c4e6c8352f1a0d
ANKIT AGARWAL <ankit.agarwal2@citrix.com>
2015-01-15 18:12:02 +0530

    fix dao

--- src/main/java/com/storage/dao/FileDaoImpl.java
+++ src/main/java/com/storage/dao/FileDaoImpl.java
@@ -3,20 +3,20 @@ package com.storage.dao;
 import java.util.Collection;
 import java.util.Map;
 
-import javax.transaction.Transactional;
-
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.storage.dto.Files;
 
 public class FileDaoImpl extends HibernateDaoSupport implements FileDao {
 
 	@Override
-	@Transactional
+	@Transactional(readOnly = false)
 	public Files createOrUpdate(Files file) {
         getHibernateTemplate().saveOrUpdate(file);
         return file;
 	}
+
 
 	@Override
 	public Collection<Files> findByParameters(Map<String, Object> parameters) {
